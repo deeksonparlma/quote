808c362adfd8a9788889bc7d2adca7c0c3f2dc16
Dickson <dicksonparlma@gmail.com>
2019-03-25 11:16:57 +0300

    date added in array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,24 @@ import { update } from './manipulate';
 export class AppComponent {
   title = 'quotes';
   voters:number[]=[0,0] ;
+  upvotes:number = 0 ;
+  downvotes:number = 0 ;
   quotes = [
-        new Manipulate(this.voters[0],this.voters[1],'Dickson','Live to your fullest potential','12:50 AM'),
-        new Manipulate(this.voters[0],this.voters[1],'Jakes','Never give up on first attempt','2:43 PM'),
-        new Manipulate(this.voters[0],this.voters[1],'Mark','Keep trying','1:50 AM'),
-        new Manipulate(this.voters[0],this.voters[1],'Juliet','It can be done only by your participation','7:39 PM'),
-        new Manipulate(this.voters[0],this.voters[1],'Job','Always seek help from your counter-part if you feel stuck','12:50 AM'),
-        new Manipulate(this.voters[0],this.voters[1],'June','Don"t be an asshole','7:43 PM'),
+        new Manipulate(this.voters[0],this.voters[1],'Dickson','Live to your fullest potential',Date()),
+        new Manipulate(this.voters[0],this.voters[1],'Jakes','Never give up on first attempt',Date()),
+        new Manipulate(this.voters[0],this.voters[1],'Mark','Keep trying',Date()),
+        new Manipulate(this.voters[0],this.voters[1],'Juliet','It can be done only by your participation',Date()),
+        new Manipulate(this.voters[0],this.voters[1],'Job','Always seek help from your counter-part if you feel stuck',Date()),
+        new Manipulate(this.voters[0],this.voters[1],'June','Don"t be an asshole',Date()),
     ]
     upvote(){
-   var Updates =  [
-       this.voters[0]++ ,
-       this.voters[1]-- ,
-     ]
-      }
+      this.upvotes +=1 ;
+}
+downvote(){
+  this.downvotes +=1 ;
+}
+
+addNewquote(quote){
+        this.quotes.push(quote)
+    }
 }
