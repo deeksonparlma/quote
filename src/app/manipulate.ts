1454a8bce76646dfb11f98fd307b330d13e839c0
Dickson <dicksonparlma@gmail.com>
2019-03-25 12:59:57 +0300

    date type added

--- src/app/manipulate.ts
+++ src/app/manipulate.ts
@@ -1,5 +1,5 @@
 export class Manipulate {
-    constructor(public upVote:number,public downVote:number,public author:string,public content:string,public time:Date){}
+    constructor(public author:string,public content:string,public time:Date){}
 }
 export class update {
     constructor(public voters:number){}
