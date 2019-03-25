e9f809271a7e8985e9a3551852e2f899799f1934
Dickson <dicksonparlma@gmail.com>
2019-03-25 11:18:09 +0300

    date property added

--- src/app/manipulate.ts
+++ src/app/manipulate.ts
@@ -1,5 +1,5 @@
 export class Manipulate {
-    constructor(public upVote:number,public downVote:number,public author:string,public content:string,public time:string){}
+    constructor(public upVote:number,public downVote:number,public author:string,public content:string,public time:Date){}
 }
 export class update {
     constructor(public voters:number){}
